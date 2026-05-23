9de178633470115a4ec6e6770a645f99f6a0e183
reham270 <rehammhmoud959@gmail.com>
2026-05-23 08:56:47 +0300

    اضافة زر تجريب

--- Forms/formShowImg.Designer.cs
+++ Forms/formShowImg.Designer.cs
@@ -34,6 +34,7 @@ namespace PixelLab
             this.ofd = new System.Windows.Forms.OpenFileDialog();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.btnQuantize = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.picImg)).BeginInit();
             this.SuspendLayout();
             // 
@@ -82,12 +83,23 @@ namespace PixelLab
             this.btnQuantize.UseVisualStyleBackColor = false;
             this.btnQuantize.Click += new System.EventHandler(this.btnQuantize_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // formShowImg
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.ClientSize = new System.Drawing.Size(1132, 624);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnQuantize);
             this.Controls.Add(this.btnImg);
             this.Controls.Add(this.picImg);
@@ -107,6 +119,7 @@ namespace PixelLab
         private System.Windows.Forms.OpenFileDialog ofd;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Button btnQuantize;
+        private System.Windows.Forms.Button button1;
     }
 }
 
